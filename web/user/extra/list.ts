8953316ffe25e771cf01c8aac3e2a5eecc08a558
peterpei1186861238 <ppzzhh1186861238@hotmail.com>
2021-08-07 22:20:18 +0800

    main

--- web/user/extra/list.ts
+++ web/user/extra/list.ts
@@ -44,7 +44,7 @@ function jsons(){
         return judge;
     }
     function initJson(){
-        if(!Object.keys(targetJson).length){getJson("https://cdn.jsdelivr.net/gh/peterpei1186861238/ASDB/db/2021/main.json");}
+        if(!Object.keys(targetJson).length){getJson("https://cdn.jsdelivr.net/gh/peterpei1186861238/ASDB@main/db/2021/main.json");}
         //列出
         var list = "";
         for (var attr in targetJson ){
