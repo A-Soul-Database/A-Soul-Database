3887286a2def3f01a3f16cc11a6e81f93e93853b
peterpei1186861238 <ppzzhh1186861238@hotmail.com>
2021-08-12 19:50:36 +0800

    update

--- web/user/extra/list.js
+++ web/user/extra/list.js
@@ -12,11 +12,19 @@ var fliter = {
     "type":[],
     "platform":[]
 }
+let stafflistAvator = new Map([
+    ["A",["https://i0.hdslb.com/bfs/face/566078c52b408571d8ae5e3bcdf57b2283024c27.jpg","Ava"]],
+    ["B",["https://i2.hdslb.com/bfs/face/668af440f8a8065743d3fa79cfa8f017905d0065.jpg","Bella"]],
+    ["C",["https://i1.hdslb.com/bfs/face/a7fea00016a8d3ffb015b6ed8647cc3ed89cbc63.jpg","Carol"]],
+    ["D",["https://i2.hdslb.com/bfs/face/d399d6f5cf7943a996ae96999ba3e6ae2a2988de.jpg","Diana"]],
+    ["E",["https://i1.hdslb.com/bfs/face/8895c87082beba1355ea4bc7f91f2786ef49e354.jpg","Ellen"]],
+    ["F",["https://i2.hdslb.com/bfs/face/f8af62422492758d3377b185869dd69a3bb0c188.jpg","Furry"]]
+]);
 var targetJson = {}
 var thumbalJson = new Map();
 //筛选索引
 
-//Get Json 
+//获取信息
 function jsons(){
 
     function getJson(url)
@@ -41,25 +49,35 @@ function jsons(){
                 break;
             }
         }
-
         return judge;
     }
+    function makeStaffChip(staffList){
+        //Make chiplist for next gen
+        var staffchip = "";
+        for(let a in staffList){
+            staffchip = `${staffchip}<div class="mdui-chip"><img class="mdui-chip-icon"src="${stafflistAvator.get(staffList[a])[0]}"/><span class="mdui-chip-title">${stafflistAvator.get(staffList[a])[1]}</span></div>`;
+        }
+        //<h6>Activity</h6><div class="mdui-chip"onclick="  mdui.snackbar({message: 'Such Art 32:59 \n Game2 33:10'});"><span class="mdui-chip-icon"><i class="mdui-icon material-icons">videogame_asset</i></span><span class="mdui-chip-title">Game</span></div><div class="mdui-chip"><span class="mdui-chip-icon"><i class="mdui-icon material-icons">mic</i></span><span class="mdui-chip-title">Song</span></div><div class="mdui-chip"><span class="mdui-chip-icon"><i class="mdui-icon material-icons">group</i></span><span class="mdui-chip-title">Chat</span></div><div class="mdui-chip"><span class="mdui-chip-icon"><i class="mdui-icon material-icons">directions_walk</i></span><span class="mdui-chip-title">Dance</span></div>
+        return staffchip;
+    }
     function initJson(){
-        if(!Object.keys(targetJson).length){targetJson =getJson("https://cdn.jsdelivr.net/gh/peterpei1186861238/ASDB@V1.0/db/2021/08/main.json");}
-        if(!Object.keys(thumbalJson).length){thumbalJson =getJson("https://asdb.live/db/2021/Cover.json");}
+        if(!Object.keys(targetJson).length){targetJson =getJson("https://cdn.jsdelivr.net/gh/peterpei1186861238/ASDB@V1.2/db/2021/main.json");}
+        if(!Object.keys(thumbalJson).length){thumbalJson =getJson("https://cdn.jsdelivr.net/gh/peterpei1186861238/ASDB@V1.2/db/2021/Cover.json");}
         //Archive for https://cdn.jsdelivr.net/gh/peterpei1186861238/ASDB@V1.0/db/2021/main.json
         //Update for https://asdb.live/db/2021/main.json
         //列出
+
         var list = "";
+        var crossJudge = true;
         for (var attr in targetJson){
-            if(!compareList(targetJson[attr]["type"],fliter["type"])){continue}
-            if(!compareList(targetJson[attr]["scene"],fliter["scene"])){continue}
-            if(!compareList(targetJson[attr]["staff"],fliter["staff"])){continue}
-            if(!compareList(targetJson[attr]["skin"],fliter["skin"])){continue}
-            if(!compareList(targetJson[attr]["platform"],fliter["platform"])){continue}
+            //筛选
+            for (let item of toolsMap.entries()){
+                if(!compareList(targetJson[attr][item.toString().split("-")[1]],fliter[item.toString().split("-")[1]])){crossJudge=false;}
+            }
+            if (!crossJudge){crossJudge=true;continue;}
             var bv = targetJson[attr]["bv"]
-            //list = `<div class="mdui-panel-item"><div class="mdui-panel-item-header">${targetJson[attr]["title"]}</div><div class="mdui-list-item-body" id="list-item-${targetJson[attr]["bv"]}"></div></div>${list}`
-            list = `<div id="test"class="mdui-col"><div class="mdui-card"><div class="mdui-card-media"><img src="${thumbalJson[bv]}"/><div class="mdui-card-media-covered mdui-card-media-covered-top"><div class="mdui-card-primary"><div class="mdui-card-primary-title">${targetJson[attr]["title"]}</div></div></div></div><div class="mdui-card-actions"><h6>Staff</h6><div class="mdui-chip"><img class="mdui-chip-icon"src="https://i0.hdslb.com/bfs/face/566078c52b408571d8ae5e3bcdf57b2283024c27.jpg@128w_128h_1o.webp"/><span class="mdui-chip-title">向晚大魔王</span></div><div class="mdui-chip"><img class="mdui-chip-icon"src="https://i2.hdslb.com/bfs/face/d399d6f5cf7943a996ae96999ba3e6ae2a2988de.jpg@128w_128h_1o.webp"/><span class="mdui-chip-title">嘉然今天吃什么</span></div><div class="mdui-chip"><img class="mdui-chip-icon"src="https://i2.hdslb.com/bfs/face/668af440f8a8065743d3fa79cfa8f017905d0065.jpg@128w_128h_1o.webp"/><span class="mdui-chip-title">贝拉Kira</span></div><h6>Activity</h6><div class="mdui-chip"onclick="  mdui.snackbar({message: 'Such Art 32:59 \n Game2 33:10'});"><span class="mdui-chip-icon"><i class="mdui-icon material-icons">videogame_asset</i></span><span class="mdui-chip-title">Game</span></div><div class="mdui-chip"><span class="mdui-chip-icon"><i class="mdui-icon material-icons">mic</i></span><span class="mdui-chip-title">Song</span></div><div class="mdui-chip"><span class="mdui-chip-icon"><i class="mdui-icon material-icons">group</i></span><span class="mdui-chip-title">Chat</span></div><div class="mdui-chip"><span class="mdui-chip-icon"><i class="mdui-icon material-icons">directions_walk</i></span><span class="mdui-chip-title">Dance</span></div><h6>Info</h6><div class="mdui-table-fluid"><table class="mdui-table"><thead><tr><th>${targetJson[attr]["date"]} ${targetJson[attr]["time"]}</th><th><a href="https://www.bilibili.com/${bv}"target="_blank">${bv}</a></th></tr></thead></table></div></div></div></div>${list}`;
+            //list = `<div id="${bv}"class="mdui-col"><div class="mdui-card" style="border-radius: 10px;"><div class="mdui-card-media"><img src="${thumbalJson[bv]}" style="height:13vw;object-fit: cover;" /><div class="mdui-card-media-covered mdui-card-media-covered-top"><div class="mdui-card-primary"><div class="mdui-card-primary-title">${targetJson[attr]["title"]}</div></div></div></div><div class="mdui-card-actions">${makeStaffChip(targetJson[attr]["staff"])}<div class="mdui-table-fluid"><table class="mdui-table"><thead><tr><th>${targetJson[attr]["date"]} ${targetJson[attr]["time"]}</th><th><a href="https://www.bilibili.com/${bv}"target="_blank">${bv}</a></th></tr></thead></table></div></div></div></div>${list}`;
+            list = `<div id="${bv}" class="mdui-col" style="transition: all 1s ease-in 1s; "><div class="mdui-card" style="border-radius: 10px;"><div class="mdui-card-media"><img src="${thumbalJson[bv]}"></div><div class="mdui-panel" style="border-radius: 10px;" mdui-panel><div class="mdui-panel-item"><div class="mdui-panel-item-header">${targetJson[attr]["title"]}<i class="mdui-panel-item-arrow mdui-icon material-icons">keyboard_arrow_down</i></div><div class="mdui-panel-item-body">${makeStaffChip(targetJson[attr]["staff"])}<table class="mdui-table"><thead><tr><th>${targetJson[attr]["date"]} ${targetJson[attr]["time"]}</th><th><a href="https://www.bilibili.com/${bv}" target="_blank">${bv}</a></th></tr></thead></table></div></div></div></div></div>${list}`;
         }
         document.getElementById('main').innerHTML = list;
         //去除dialog
@@ -83,9 +101,7 @@ function filter(){
             //赋值
         }
     }
-
     for(let attr of toolsMap.values()){
-
         var child = document.getElementById(attr).firstChild;
         var last = document.getElementById(attr).lastChild;
         while(child!=last){
