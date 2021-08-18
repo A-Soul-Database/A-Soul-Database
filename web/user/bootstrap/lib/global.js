86d156fe3db2ce673b473797e18f290dd35a55f2
peterpei1186861238 <ppzzhh1186861238@hotmail.com>
2021-08-18 23:12:18 +0800

    update

--- web/user/bootstrap/lib/global.js
+++ web/user/bootstrap/lib/global.js
@@ -1,4 +1,4 @@
-var build = "V1.24";
+var build = "V1.25";
 var lastUpdateTime = "21-08-17 15:30";
 var source = ["https://cdn.jsdelivr.net/gh/peterpei1186861238/ASDB@"+build,"https://asdb.live","https://github.com/peterpei1186861238/ASDB/tree/"+build];
 var sourceUrl = source[0];
