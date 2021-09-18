332cda6a080810ed87cd0b18945706ed75ed89ba
peter_pei <32994395+peterpei1186861238@users.noreply.github.com>
2021-09-18 20:04:29 +0800

    Update main.py

--- tools/sendWebhook/main.py
+++ tools/sendWebhook/main.py
@@ -21,7 +21,7 @@ def main():
             result["db"] = True
         if i.split("/")[0] == "make-front-end":
             result["web"] = True
-    with open("./sendList.txt","r",encoding="utf-8")as f:
+    with open("tools/sendWebhook/sendList.txt","r",encoding="utf-8")as f:
         urlList = f.read().split("\n")
         f.close()
     for i in urlList:
