0accc6e5ae613067478499f36485975de9144caa
peterpei1186861238 <ppzzhh1186861238@hotmail.com>
2021-10-25 12:12:54 +0800

    test

--- tools/GithubActions/sendWebhook/sendHooks.py
+++ tools/GithubActions/sendWebhook/sendHooks.py
@@ -9,7 +9,7 @@ def sendWebhooks():
     """
     differ = sys.argv[1]
     differ = json.loads(differ)
-    TargetWebList = open("tools/sendWebhook/sendList.txt").read().split("\n")
+    TargetWebList = open("tools/GithubActions/sendWebhook/sendList.txt").read().split("\n")
     for i in TargetWebList:
         try:
             print(f"trying to send webhook to{i}")
